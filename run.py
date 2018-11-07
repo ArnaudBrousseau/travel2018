7d18c0fdfe33d86c03c8b55a5f67b69cc45b44ad
Arnaud Brousseau <arnaud.brousseau@gmail.com>
2018-11-07 15:53:26 -0800

    Initial commit of the map generation.

---
+++ run.py
@@ -0,0 +1,15 @@
+#!venv/bin/python
+import mapnik
+
+stylesheet = 'world_style.xml'
+image = 'output.png'
+mapnik.register_fonts('/Library/Fonts/');
+
+#for face in mapnik.FontEngine.face_names(): print face
+
+m = mapnik.Map(2400, 1200)
+mapnik.load_map(m, stylesheet)
+m.zoom_all()
+#m.pan_and_zoom(1000, 400, 0.4)
+mapnik.render_to_file(m, image)
+print "rendered image to '%s'" % image
