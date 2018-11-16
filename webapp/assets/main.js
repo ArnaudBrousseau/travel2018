a055ce7fb7de05a4bc55c3d8793aecc3175c45cb
Arnaud Brousseau <arnaud.brousseau@gmail.com>
2018-11-15 19:07:35 -0800

    Adds event handler for time slider.

--- webapp/assets/main.js
+++ webapp/assets/main.js
@@ -1,9 +1,10 @@
-console.log('√');
+console.log('2018, a roller-coaster');
 
 var featureDetection = function() {
   // TODO: feature detection for:
   // * getElementById
 }
+
 /**
  * Adds animations, e.g.
  *    <animateMotion xlink:href="#arnaud-sad-circle" dur="0.7s" begin="0s" fill="freeze" id="one">
@@ -16,3 +17,18 @@ var animate = function(element_id, path_id) {
   // TODO: find a way to add SVG animations on the fly, or programatically animate an object along a path.
 };
 animate('arnaud-sad-circle', 'sf-to-paris');
+
+var setUpSlider = function() {
+  document.getElementById('day-slider').addEventListener('change', onSliderChange);
+};
+
+var onSliderChange = function(e) {
+  /**
+   * TODO: get the corresponding positions for both of us
+   * then highlight the active paths,
+   * then animate to the new position and update the avatars
+   */
+  console.log(e.target.value);
+};
+
+setUpSlider();
